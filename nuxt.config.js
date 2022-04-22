77daab5e0e17a8674faf7468cc7166b80ea8ede3
khafiluhur <khafiluhur.99@gmail.com>
2022-04-22 07:51:19 +0700

    proxy axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,6 +74,7 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: process.env.API_URL,
     https: false,
+    proxy: true,
     headers: {
       common: {
         Authorization: process.env.API_KEY,
